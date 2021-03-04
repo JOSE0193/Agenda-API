b9d637b8e4e1e30eb58486e8bc8c203d87d0820d
Jose Antonio <jose01.11.93@gmail.com>
2021-03-04 09:53:01 -0300

    correção de build

--- src/main/java/com/limaconsultoria/agendaapi/agendaapi/util/DateUtil.java
+++ src/main/java/com/limaconsultoria/agendaapi/agendaapi/util/DateUtil.java
@@ -1,9 +1,12 @@
 package com.limaconsultoria.agendaapi.agendaapi.util;
 
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class DateUtil {
     public String formatLocalDateTimeToDataBaseStyle(LocalDateTime localDateTime){
         return DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").format(localDateTime);
